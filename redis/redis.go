b90ac40d86a06a95c438ac72853372344c9eabc6
drzhangg <654014730@qq.com>
2020-11-01 21:22:31 +0800

    test redis

--- redis/redis.go
+++ redis/redis.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"context"
+	"github.com/go-redis/redis"
+)
+
+var ctx = context.Background()
+
 func main() {
-	
+	client := redis.NewClient(&redis.Options{
+		Addr: "192.168.31.191:6379",
+	})
+
+	client.Set("name", "jerry", 0)
 }
