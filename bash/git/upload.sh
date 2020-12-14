1e4db809fcfee53ea3b598581e0bf210a54b5a16
drzhangg <654014730@qq.com>
2020-12-14 16:51:22 +0800

    channel

--- bash/git/upload.sh
+++ bash/git/upload.sh
@@ -1,4 +1,8 @@
+#/bin/sh
+
 cd D:/godemo/go_base_util
+
+pwd
 
 git add .
 
@@ -6,6 +10,6 @@ git config --global user.name "drzhangg"
 
 git config --global user.email "654014730@qq.com"
 
-git commit -am '$1'
+git commit -am "$1"
 
 git push
