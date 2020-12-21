20339fd97ec5b7e71adc64692da2c4bd86db433b
drzhangg <654014730@qq.com>
2020-12-21 20:27:36 +0800

    modify

--- channel/demo1/main.go
+++ channel/demo1/main.go
@@ -5,11 +5,20 @@ import (
 	"time"
 )
 
+type Format int
+
+const (
+	B Format = (iota + 1) * 2
+	A
+)
+
 func hello() {
 	fmt.Println("this is a goroutine")
 }
 
 func main() {
+	fmt.Println(B)
+	fmt.Println(A)
 	go hello()
 
 	time.Sleep(time.Second)
