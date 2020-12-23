f8f48f6e32f533f8230b441cceb6ee9d56b530bd
drzhangg <654014730@qq.com>
2020-12-23 09:39:16 +0800

    add nsq

--- nsq/demo/producer/main.go
+++ nsq/demo/producer/main.go
@@ -1,39 +1,41 @@
+// gin-vue/product.go
 package main
 
 import (
-	"fmt"
 	"github.com/nsqio/go-nsq"
 	"go_base_util/private"
+	"io/ioutil"
+	"log"
+	"strconv"
 )
 
-func main() {
-	//定义nsq生产者
-	var producer *nsq.Producer
-	//初始化生产者
-	producer, err := nsq.NewProducer(private.ADDR+":4150", nsq.NewConfig())
+var nullLogger = log.New(ioutil.Discard, "", log.LstdFlags)
+
+func sendMessage() {
+	config := nsq.NewConfig() // 1. 创建生产者
+	producer, err := nsq.NewProducer(private.ADDR + ":4150", config)
 	if err != nil {
-		panic(err)
+		log.Fatalln("连接失败: (127.0.0.1:4150)", err)
 	}
 
-	err = producer.Ping()
-	if err != nil {
-		//关闭生产者
-		producer.Stop()
-		producer = nil
+	errPing := producer.Ping() // 2. 生产者ping
+	if errPing != nil {
+		log.Fatalln("无法ping通: 127.0.0.1:4150", errPing)
 	}
 
-	topic := "test_topic"
+	producer.SetLogger(nullLogger, nsq.LogLevelInfo) // 3. 设置不输出info级别的日志
 
-	for i := 0; i < 10; i++ {
-		message := fmt.Sprintf("message:%d", i)
-
-		if producer != nil && message != "" { //不能发布空串，会导致error
-			err = producer.Publish(topic, []byte(message))
-			if err != nil {
-				fmt.Printf("producer publish err:%v", err)
-			}
-			fmt.Println(message)
+	for i := 0; i < 5; i++ { // 4. 生产者发布消息
+		message := "消息发送测试 " + strconv.Itoa(i+10000)
+		err2 := producer.Publish("one-test", []byte(message)) // 注意one-test　对应消费者consumer.go　保持一致
+		if err2 != nil {
+			log.Panic("生产者推送消息失败!")
 		}
 	}
-	fmt.Println("producer Publish success")
+
+	producer.Stop() // 5. 生产者停止执行
+}
+
+func main() {
+	sendMessage()
 }
