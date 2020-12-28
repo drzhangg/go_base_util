0c9886913e5321ae814d4e058e34201dcbcd8ffa
drzhangg <654014730@qq.com>
2020-12-28 17:18:41 +0800

    add test remove

--- gee/gee_cache/lru/lru_test.go
+++ gee/gee_cache/lru/lru_test.go
@@ -9,14 +9,27 @@ func (d String) Len() int {
 }
 
 func TestGet(t *testing.T) {
-	lru := New(int64(0),nil)
-	lru.Add("key1",String("12345"))
-	if v,ok := lru.Get("key1");!ok || string(v.(String)) != "12345"{
+	lru := New(int64(0), nil)
+	lru.Add("key1", String("12345"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "12345" {
 		t.Fatalf("cache hit key1=12345 failed")
 	}
 
 	if _, ok := lru.Get("key2"); ok {
 		t.Fatalf("cache miss key2 failed")
 	}
+}
 
+func TestRemoveolddest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value1", "value1"
+	cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
 }
