ef1d8414be3b80820bcb9ba059964e2c41108eee
drzhangg <654014730@qq.com>
2020-12-28 11:16:43 +0800

    add trie

--- gee/gee_web/day1-http-base/base3/gee/trie.go
+++ gee/gee_web/day1-http-base/base3/gee/trie.go
@@ -1,5 +1,7 @@
 package gee
 
+import "strings"
+
 type node struct {
 	pattern  string  //待匹配路由，例如：/p/:lang
 	part     string  //路由中的一部分，例如：:lang
@@ -7,6 +9,7 @@ type node struct {
 	isWild   bool    //是否精确匹配，part含有：或*时为true
 }
 
+// 第一个匹配成功的节点，用于插入
 func (n *node) matchChild(part string) *node {
 	for _, child := range n.children {
 		if child.part == part || child.isWild {
@@ -16,6 +19,7 @@ func (n *node) matchChild(part string) *node {
 	return nil
 }
 
+// 所有匹配成功的节点，用于查找
 func (n *node) matchChildren(part string) []*node {
 	nodes := make([]*node, 0)
 	for _, child := range n.children {
@@ -24,4 +28,42 @@ func (n *node) matchChildren(part string) []*node {
 		}
 	}
 	return nodes
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{
+			part:   part,
+			isWild: part[0] == ':' || part[0] == '*',
+		}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+	return nil
 }
