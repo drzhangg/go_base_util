0c396fb3f15b020c3a72a597b59632bf27a8335c
drzhangg <654014730@qq.com>
2020-12-29 16:31:26 +0800

    add gee_cache

---
+++ gee/gee_cache/byteview.go
@@ -0,0 +1,24 @@
+package gee_cache
+
+type ByteView struct {
+	b []byte
+}
+
+// Len returns the view's length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
