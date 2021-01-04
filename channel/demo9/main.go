a9b73b15eda11385ea333d027677dcb141690300
drzhangg <654014730@qq.com>
2021-01-04 09:43:32 +0800

    add gee_rpc

--- channel/demo9/main.go
+++ channel/demo9/main.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func test(i []int, x int) () {
 	i = append(i, x)
+	fmt.Printf("%p\n",i)
 	fmt.Println("dfsdfa", i)
 }
 
 func main() {
 	var i = make([]int, 0, 10)
 	test(i, 15)
+	fmt.Printf("%p\n",i)
 	fmt.Println(i)
 }
