a2c1bb5bb9bbbbfc6e7cdbf8545f56904b6538c7
zhang <654014730@qq.com>
2021-03-11 10:01:47 +0800

    leetcode 两数之和

---
+++ data_struct/leetcode_1/数组/09_两数之和.go
@@ -0,0 +1,24 @@
+package 数组
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum1(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+
+	return nil
+}
