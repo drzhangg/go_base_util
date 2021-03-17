c24d5da4fed817d75b9aa68015ae7af700afb1c8
zhang <654014730@qq.com>
2021-03-17 10:06:17 +0800

    leetcode 字符串中的第一个唯一字符

---
+++ data_struct/leetcode_1/字符串/03_字符串中的第一个唯一字符.go
@@ -0,0 +1,15 @@
+package 字符串
+
+func firstUniqChar(s string) int {
+	cnt := [26]int{}
+	for _,ch := range s{
+		cnt[ch - 'a']++
+	}
+
+	for i,ch := range s{
+		if cnt[ch - 'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}
