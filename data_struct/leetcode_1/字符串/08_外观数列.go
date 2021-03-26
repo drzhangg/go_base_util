2f264b210ae89916756caf2215150195c6410c2f
zhang <654014730@qq.com>
2021-03-26 12:38:36 +0800

    leetcode 外观数列"

---
+++ data_struct/leetcode_1/字符串/08_外观数列.go
@@ -0,0 +1,29 @@
+package 字符串
+
+import (
+	"strconv"
+	"strings"
+)
+
+func countAndSay(n int) string {
+	str := "1"
+	for i := 2; i <= n; i++ {
+		var tmp strings.Builder
+		preByte := str[0]
+		count := 1
+		for j := 1; j < len(str); j++ {
+			if str[j] == preByte {
+				count++
+			} else {
+				tmp.WriteString(strconv.Itoa(count))
+				tmp.WriteByte(preByte)
+				preByte = str[j]
+				count = 1
+			}
+		}
+		tmp.WriteString(strconv.Itoa(count))
+		tmp.WriteByte(preByte)
+		str = tmp.String()
+	}
+	return str
+}
