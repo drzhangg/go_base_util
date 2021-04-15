85c8136b5269f957505dc1e14ef0fc98455131d0
zhang <654014730@qq.com>
2021-04-15 13:58:09 +0800

    leetcode 打家劫舍

--- map/demo2/main.go
+++ map/demo2/main.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var ms []map[string]interface{}
-	for i := 0; i < 10; i++ {
-		m := make(map[string]interface{})
-		m["key_"+fmt.Sprint(i)] = i
-		ms = append(ms, m)
-	}
-	fmt.Println(ms)
+	//var ms []map[string]interface{}
+	//for i := 0; i < 10; i++ {
+	//	m := make(map[string]interface{})
+	//	m["key_"+fmt.Sprint(i)] = i
+	//	ms = append(ms, m)
+	//}
+	//fmt.Println(ms)
+
+	var m = make(map[string]interface{})
+
+	m["a"] = 0
+	fmt.Println(m)
+
+	m["a"] = 1
+	fmt.Println(m)
 }
