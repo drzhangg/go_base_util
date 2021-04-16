eee20e7eb645bce6bc31f2fe038a6ac41b064f3a
zhang <654014730@qq.com>
2021-04-16 09:51:41 +0800

    leetcode 寻找数组的中心索引

---
+++ data_struct/leetcode_2/数组/01_寻找数组的中心索引.go
@@ -0,0 +1,16 @@
+package 数组
+
+func pivotIndex(nums []int) int {
+	total := 0
+	for _,v := range nums{
+		total+=v
+	}
+	sum := 0
+	for i,v := range nums{
+		if 2 *sum+v == total{
+			return i
+		}
+		sum+=v
+	}
+	return -1
+}
--- map/demo2/main.go
+++ map/demo2/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 func main() {
 	//var ms []map[string]interface{}
@@ -11,11 +14,39 @@ func main() {
 	//}
 	//fmt.Println(ms)
 
-	var m = make(map[string]interface{})
 
-	m["a"] = 0
-	fmt.Println(m)
 
-	m["a"] = 1
-	fmt.Println(m)
+	var s = "http://100.73.70.169:7777/file-manage-bucket/2021-03-31/b2fe0403-a420-4aab-8a54-2ad7e6d1523e.sh"
+	reg := regexp.MustCompile(`^http://([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}):([0-9]{1,5})/([\w-]+)/([\w-]+)/([\w-]+).([\w]+)$`)
+	//s1 := reg.ReplaceAllString(s,"")
+	ss := reg.FindStringSubmatch(s)
+	fmt.Println(len(ss))
+	fmt.Println(ss)
+	//fmt.Println(s1)
+
+	//reg1 := regexp.MustCompile(`^http://www.flysnow.org/([\d]{4})/([\d]{2})/([\d]{2})/(([\w-]+).([\w]+))$`)
+	//s2 := reg1.FindStringSubmatch(s)
+	//fmt.Println(s2)
+
+	flysnowRegexp := regexp.MustCompile(`^http://www.flysnow.org/([\d]{4})/([\d]{2})/([\d]{2})/([\w-]+).html$`)
+	params := flysnowRegexp.FindStringSubmatch("http://www.flysnow.org/2018/01/20/golang1-goque2ry-examples1-selector.html")
+
+	for _,param :=range params {
+		fmt.Println(param)
+	}
+
+
+	files := regexp.MustCompile(`^/([\w-]+)/(([\w-]+).([\w]+))$`)
+	pp := files.FindStringSubmatch("/us2r-loca3l/ab123c.sh")
+	fmt.Println(pp)
+
+}
+
+func IsIP(ip string) (b bool) {
+	m, _ := regexp.MatchString(`^http://[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$`, ip)
+	if !m {
+		return false
+	} else {
+		return true
+	}
 }
