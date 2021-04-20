609db62658b0dfa60b346696b760e1f9d4ec9490
zhang <654014730@qq.com>
2021-04-20 14:14:53 +0800

    leetcode 合并区间

--- map/demo2/main.go
+++ map/demo2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -15,8 +16,13 @@ func main() {
 	//fmt.Println(ms)
 
 
+	var s = "http://100.73.70.169:7777/file-manage-bucket/2021-03-31/b2fe0403-a420-4aab-8a54-2ad7e6d1523e.s"
 
-	var s = "http://100.73.70.169:7777/file-manage-bucket/2021-03-31/b2fe0403-a420-4aab-8a54-2ad7e6d1523e.sh"
+
+	as := strings.SplitN(s,"/",-1)
+	fmt.Println(as)
+
+
 	reg := regexp.MustCompile(`^http://100.73.70.169:7777/([\w-]+)/([\w-]+)/([\w-]+).([\w]+)$`)
 	//s1 := reg.ReplaceAllString(s,"")
 	ss := reg.FindStringSubmatch(s)
@@ -28,17 +34,19 @@ func main() {
 	//s2 := reg1.FindStringSubmatch(s)
 	//fmt.Println(s2)
 
-	flysnowRegexp := regexp.MustCompile(`^http://www.flysnow.org/([\d]{4})/([\d]{2})/([\d]{2})/([\w-]+).html$`)
-	params := flysnowRegexp.FindStringSubmatch("http://www.flysnow.org/2018/01/20/golang1-goque2ry-examples1-selector.html")
+	//flysnowRegexp := regexp.MustCompile(`^http://www.flysnow.org/([\d]{4})/([\d]{2})/([\d]{2})/([\w-]+).html$`)
+	//params := flysnowRegexp.FindStringSubmatch("http://www.flysnow.org/2018/01/20/golang1-goque2ry-examples1-selector.html")
+	//
+	//for _,param :=range params {
+	//	fmt.Println(param)
+	//}
 
-	for _,param :=range params {
-		fmt.Println(param)
-	}
 
+	//files := regexp.MustCompile(`^/([\w-]+)/(([\w-]+).([\w]+))$`)
+	//pp := files.FindStringSubmatch("/us2r-loca3l/ab123c.sh")
+	//fmt.Println(pp)
 
-	files := regexp.MustCompile(`^/([\w-]+)/(([\w-]+).([\w]+))$`)
-	pp := files.FindStringSubmatch("/us2r-loca3l/ab123c.sh")
-	fmt.Println(pp)
+	//strings.Replace(url.Path, "/"+config.OssConfig.FileBucket+"/", "", 1)
 
 }
 
