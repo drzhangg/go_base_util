0eddbbf74a18f520e1b7ab4216f7340c539188dd
zhang <654014730@qq.com>
2021-04-27 09:46:46 +0800

    leetcode 好数对的数目

--- data_struct/leetcode/1512_好数对的数目.go
+++ data_struct/leetcode/1512_好数对的数目.go
@@ -1,16 +1,13 @@
 package leetcode
 
-import "fmt"
-
 func numIdenticalPairs(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
 	var count int
 	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums)-i; j++ {
+		for j := i + 1; j < len(nums); j++ {
 			if nums[i] == nums[j] && i < j {
-				fmt.Println(i, j)
 				count++
 			}
 		}
