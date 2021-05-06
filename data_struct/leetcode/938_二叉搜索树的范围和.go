dfb164e39980354d525b145174d51bbb94025fac
zhang <654014730@qq.com>
2021-05-06 09:39:25 +0800

    广度优先搜索

--- data_struct/leetcode/938_二叉搜索树的范围和.go
+++ data_struct/leetcode/938_二叉搜索树的范围和.go
@@ -21,3 +21,25 @@ func rangeSumBST(root *TreeNode, low, high int) int {
 
 	return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
 }
+
+func rangeSumBST1(root *TreeNode, low, high int) int {
+	q := []*TreeNode{root}
+	var sum int
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		if node == nil {
+			continue
+		}
+
+		if node.Val > high {
+			q = append(q, node.Left)
+		} else if node.Val < low {
+			q = append(q, node.Right)
+		} else {
+			sum += node.Val
+			q = append(q, node.Left, node.Right)
+		}
+	}
+	return sum
+}
