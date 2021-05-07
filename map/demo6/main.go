149642b12176e6793c111d289eaba24842d716ca
zhang <654014730@qq.com>
2021-05-07 09:33:37 +0800

    leetcode 2的幂

--- map/demo6/main.go
+++ map/demo6/main.go
@@ -13,7 +13,14 @@ type ListNode struct {
 func main() {
 
 	a := "1010"
-	i,_ := strconv.ParseInt(a,2,32)
+	i, _ := strconv.ParseInt(a, 2, 32)
 	fmt.Println(i)
 
+	fmt.Println(16 >> 5)
+	fmt.Println(16 << 2)
+
+}
+
+func isPowerOfTwo(n int) bool {
+	return n > 0 && n&(n-1) == 0
 }
