3f42942ca0a9c92397f0b678d0042b9debd4bacb
zhang <654014730@qq.com>
2021-05-11 09:53:50 +0800

    leetcode 反转链表

--- map/demo7/main.go
+++ map/demo7/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(16 >>1)
+	var s1 = "123456789"
+	var s2 = s1
+
+	//fmt.Println(len(s2))
+	//fmt.Println(1<<9)
+	var a1 byte = 1 << len(s1) / 128
+	var a2 byte = 1 << len(s2) / 128
+
+	fmt.Println(a1,a2)
+
+	fmt.Println(1^2)
 }
 
 
