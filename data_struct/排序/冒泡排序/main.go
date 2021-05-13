33042f64a870004e89c2d1faf9f324f43c52c725
zhang <654014730@qq.com>
2021-05-13 22:37:55 +0800

    冒泡排序

--- data_struct/排序/冒泡排序/main.go
+++ data_struct/排序/冒泡排序/main.go
@@ -9,9 +9,9 @@ func main() {
 
 func BubbleSort(arr []int) []int {
 	for i := 0; i < len(arr); i++ {
-		for j := i + 1; j < len(arr)-1; j++ {
-			if arr[i] > arr[j] {
-				arr[i], arr[j] = arr[j], arr[i]
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
