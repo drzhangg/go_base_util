d3a2bc60df2255dddffec208b487e254c8570bfd
zhang <654014730@qq.com>
2021-05-13 18:44:07 +0800

    选择排序

--- data_struct/排序/选择排序/main.go
+++ data_struct/排序/选择排序/main.go
@@ -8,18 +8,21 @@ func main() {
 }
 
 func selectSort(arr []int) []int {
+	/*
+	1.将第一个元素的设置成最小值
+	2.依次往后进行比较，如果后面的值小于最小值，更新最小值下标
+	3.继续往后进行对比
+	 */
 	for i := 0; i < len(arr); i++ {
-		//min := i
-		min := arr[i]
+		min := i
 		for j := i + 1; j < len(arr); j++ {
-			if min > arr[j] {
-				min = arr[j]
+			if arr[min] > arr[j] {
+				min = j
 			}
-			temp := arr[i]
-			arr[i] = arr[j]
-			arr[j] = temp
 		}
-
+		temp := arr[min]
+		arr[min] = arr[i]
+		arr[i] = temp
 	}
 	return arr
 }
