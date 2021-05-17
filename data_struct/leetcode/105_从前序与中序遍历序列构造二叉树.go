30b8dcad03ceb4aaf8b2c658caa86cf9df512613
zhang <654014730@qq.com>
2021-05-17 13:09:50 +0800

    leetcode 从前序与中序遍历序列构造二叉树

--- data_struct/leetcode/105_从前序与中序遍历序列构造二叉树.go
+++ data_struct/leetcode/105_从前序与中序遍历序列构造二叉树.go
@@ -1,5 +1,30 @@
 package leetcode
 
-//func buildTree(preorder []int, inorder []int) *TreeNode {
-//
-//}
+/*
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+*/
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	//前序遍历的第一个值，是二叉树的根节点
+	root := &TreeNode{preorder[0], nil, nil}
+	i := 0
+	for ; i < len(preorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	//这里i在中序遍历里是根节点的下标，左边是左子树，有边是右子树
+
+	//左子树的长度
+	ll := len(inorder[:i])
+	//右子树的长度
+
+	//左子树
+	root.Left = buildTree(preorder[1:ll+1],inorder[:i])
+	root.Right = buildTree(preorder[1+ll:],inorder[i+1:])
+	return root
+}
--- data_struct/leetcode/74_搜索二维矩阵.go
+++ data_struct/leetcode/74_搜索二维矩阵.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "sort"
+
 func searchMatrix(matrix [][]int, target int) bool {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[i]); j++ {
@@ -9,4 +11,25 @@ func searchMatrix(matrix [][]int, target int) bool {
 		}
 	}
 	return false
+}
+
+func searchMatrix2(matrix [][]int, target int) bool {
+	row := sort.Search(len(matrix), func(i int) bool {
+		return matrix[i][0] > target
+	}) - 1
+
+	if row < 0 {
+		return false
+	}
+
+	col := sort.SearchInts(matrix[row], target)
+	return col < len(matrix[row]) && matrix[row][col] == target
+}
+
+func searchMatrix3(matrix [][]int, target int) bool {
+	m,n := len(matrix),len(matrix[0])
+	i := sort.Search(m*n , func(i int) bool {
+		return matrix[i/n][i%n] >=target
+	})
+	return i< m*n &&matrix[i/n][i%n]==target
 }
