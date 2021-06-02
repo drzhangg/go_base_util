850427d4e73ee132e7f4ce15ac882b0297e7ac75
zhang <654014730@qq.com>
2021-06-02 10:18:10 +0800

    leetcode 最大子序和"

---
+++ data_struct/leetcode/53_最大子序和.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] + nums[i-1] > nums[i]{
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max{
+			max = nums[i]
+		}
+	}
+	return max
+}
+
+func getMax(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
